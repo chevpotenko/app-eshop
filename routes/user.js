adcb519e4af0c3105dea3a2a6f26925fe253ad11
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-02-23 14:45:45 +0200

    refactor(routes/user)

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,24 @@
-var express = require('express');
-var path = require('path');
-var router = express.Router();
-var passport = require('passport');
+const express = require('express');
+const path = require('path');
+const router = express.Router();
 
-router.get('/profile', isLoggedIn, function(req, res, next) {  
+router.get('/profile', isLoggedIn, function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', '/public/index.html'));
 });
 
-router.use('/', notLoggedIn, function(req, res, next) {  
+router.use('/', notLoggedIn, function(req, res, next) {
   next();
 });
 
-router.get('/signup', function(req, res, next) {  
+router.get('/signup', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', '/public/index.html'));
 });
 
-router.get('/signin', function(req, res, next) {  
+router.get('/signin', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', '/public/index.html'));
 });
 
-router.get('/logout', function(req, res, next) {  
+router.get('/logout', function(req, res, next) {
   req.logout();
   res.redirect('/')
 });
