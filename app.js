40e89277f641cff044ce01e4f147117762b54985
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-02-23 15:21:11 +0200

    update(app): cors

--- app.js
+++ app.js
@@ -47,9 +47,16 @@ app.use(session({
 }));
 
 app.use(csrf());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(function(req, res, next) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
-  return next();
+  next();
 });
 
 app.use(passport.initialize());
