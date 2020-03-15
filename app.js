cbd91057ec0f53707ee5123b3417337aa1f3cae4
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-03-15 20:54:48 +0200

    update(app): delete middleware

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const validator = require('express-validator');
-const csurf = require('csurf');
 
 const app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,17 +14,13 @@ app.use(require('./middlewares/cors'));
 app.use(logger('dev'));
 app.use(validator());
 app.use(require('./middlewares/session'));
-require('./passport/passport')(app);
-app.use(require('./middlewares/authenticated'));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 require('./utils/init-api')(app);
 app.use('/', require('./routes/index'));
 
-app.use(csurf({ cookie: true }));
-app.use(require('./middlewares/csurf'));
 app.use(require('./middlewares/errors'));
 
 module.exports = app;
--- middlewares/authenticated.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(req, res, next) {
-    res.locals.login = req.isAuthenticated();
-    res.locals.session = req.session;
-    next();
-};
